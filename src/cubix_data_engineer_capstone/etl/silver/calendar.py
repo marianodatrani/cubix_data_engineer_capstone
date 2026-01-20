cff77ea294420abc2250ed5b959e5011ed94ecfb
Mariano da Trani <haengmeister@gmail.com>
2026-01-20 22:33:48 +0100

    created silver folder and added init.py and calendar.py

---
+++ src/cubix_data_engineer_capstone/etl/silver/calendar.py
@@ -0,0 +1,34 @@
+import pyspark.sql.functions as sf
+from pyspark.sql import DataFrame
+
+
+def get_calendar(calendar_raw: DataFrame) -> DataFrame:
+    """_summary_
+
+    Args:
+        calendar_raw (DataFrame): _description_
+
+    Returns:
+        DataFrame: _description_
+    """
+
+    calendar = (
+        calendar_raw
+        .select(
+            sf.col("Date").cast("date"),
+            sf.col("DayNumberOfWeek").cast("int"),
+            sf.col("DayName"),
+            sf.col("MonthName"),
+            sf.col("MonthNumberOfYear").cast("int"),
+            sf.col("DayNumberOfYear").cast("int"),
+            sf.col("WeekNumberOfYear").cast("int"),
+            sf.col("CalendarQuarter").cast("int"),
+            sf.col("CalendarYear").cast("int"),
+            sf.col("FiscalYear").cast("int"),
+            sf.col("FiscalSemester").cast("int"),
+            sf.col("FiscalQuarter").cast("int"),
+            sf.col("FinMonthNumberOfYear").cast("int"),
+            sf.col("DayNumberOfMonth").cast("int"),
+            sf.col("MonthID").cast("int"),
+        )
+    )
