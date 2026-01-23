63d2eded9e4b81614248962268a08b71babd6432
Mariano da Trani <haengmeister@gmail.com>
2026-01-23 22:19:49 +0100

    built 0.1.4 wheel file and added scd.py

---
+++ src/cubix_data_engineer_capstone/etl/silver/scd.py
@@ -0,0 +1,26 @@
+from delta.tables import DeltaTable  # type: ignore
+from pyspark.sql import DataFrame, SparkSession
+
+
+def scd1_uc(spark: SparkSession, table_name: str, new_data: DataFrame, primary_key: str):
+    """Slowly Changing Dimension Type 1 fro UC Volumes.
+    Compares the master Delta table with new_data, updating or inserting as needed.
+
+    :param spark:           SparkSession.
+    :param table_name:      Name of the table.
+    :param new_data:        DataFrame with the new data.
+    :param primary_key:     Param name used as priomary key.
+    """
+    delta_master = DeltaTable.forName(spark, table_name)
+
+    (
+        delta_master
+        .alias("master")
+        .merge(
+            new_data.alias("new_data"),
+            f"master.{primary_key} = new_data.{primary_key}"
+        )
+        .whenMatchedUpdateAll()
+        .whenNotMatchedInsertAll()
+        .execute()
+    )
